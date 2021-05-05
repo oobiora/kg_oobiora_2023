42b50c7d428b42c2c2c676f23ce9d61fd9dcb5c5
Olisa Obiora <oobiora22@gmail.com>
2021-05-04 22:47:18 -0400

    condensing and documenting
    Condensing and documenting final code

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 
-
-
 const convert = (values) => {
-
     for (let i in values) {
-        word = [...values[i] + ''].map(Number)
-        process.stdout.write(word.map(x => phonetic_table[x]).join('') + " ")
-    }
-    
+        process.stdout.write([...values[i] + ''].map(Number).map(x => phonetic_table[x]).join('') + " ") // spread value at index map to phonetic table and rejoin word and print to std.out
+    }  
 }
 const phonetic_table = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 convert(process.argv.slice(2))
